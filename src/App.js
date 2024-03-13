bef9f5b3103e8986207c9f90544a2c7c6e0fcaca
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-03-14 01:44:59 +0530

    Created a CUSTOM HOOK to fetch the movie data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import StarRating from "./StarRating";
+import { useMovies } from "./useMovies";
 
 const average = (arr) => {
   return arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -10,10 +11,9 @@ const KEY = "e1685ece";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedID, setSelectedID] = useState(null);
+
+  const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
 
   // const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
@@ -44,55 +44,6 @@ export default function App() {
       localStorage.setItem("watched", JSON.stringify(watched));
     },
     [watched]
-  );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-
-          // console.log(data);
-
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.error(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-      handleCloseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
   );
 
   return (
@@ -315,6 +266,7 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
       runtime: isNaN(Number(runtime.split(" ").at(0)))
         ? "NA"
         : Number(runtime.split(" ").at(0)),
+
       countRatingDecisions: countRef.current,
     };
 
